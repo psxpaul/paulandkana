474ca7cb898016f5681764caa39adfb59dbba895
Paul Roberts <proberts@cj.com>
2013-05-17 16:43:12 -0700

    Fixing some issues with mongodb and bumping some versions

--- Comment.js
+++ Comment.js
@@ -1,29 +1,30 @@
 var MongoDb = require("mongodb"),
     ObjectID = MongoDb.ObjectID,
-    db = new MongoDb.Db("test", new MongoDb.Server("localhost", 27017, {auto_reconnect: true}, {}));
+    Db = MongoDb.Db,
+    db;
 
-db.open(function (err, db) {});
+Db.connect("mongodb://localhost:27017/?w=1", function (err, db) {
+    exports.findForArticle = function (articleQuery, callback) {
+        db.collection("comments", function (err, collection) {
+            collection.find({article: articleQuery}).toArray(callback);
+        });
+    };
 
-exports.findForArticle = function (articleQuery, callback) {
-    db.collection("comments", function (err, collection) {
-        collection.find({article: articleQuery}).toArray(callback);
-    });
-};
+    exports.findById = function (id, callback) {
+        db.collection("comments", function (error, collection) {
+            collection.findOne({_id: new ObjectID(id)}, callback);
+        });
+    };
 
-exports.findById = function (id, callback) {
-    db.collection("comments", function (error, collection) {
-        collection.findOne({_id: new ObjectID(id)}, callback);
-    });
-};
+    exports.save = function (input, callback) {
+        input.date = new Date();
 
-exports.save = function (input, callback) {
-    input.date = new Date();
+        if (input._id) {
+            input._id = new ObjectID(input._id);
+        }
 
-    if (input._id) {
-        input._id = new ObjectID(input._id);
-    }
-
-    db.collection("comments", function (error, collection) {
-        collection.save(input, {safe: true}, callback);
-    });
-};
+        db.collection("comments", function (error, collection) {
+            collection.save(input, {safe: true}, callback);
+        });
+    };
+});
--- Guest.js
+++ Guest.js
@@ -1,50 +1,50 @@
 var MongoDb = require("mongodb"),
     ObjectID = MongoDb.ObjectID,
-    db = new MongoDb.Db("test", new MongoDb.Server("localhost", 27017, {auto_reconnect: true}, {})),
+    Db = MongoDb.Db,
     email   = require("./node_modules/emailjs/email"),
-    server  = email.server.connect({
+    emailServer  = email.server.connect({
         user: "pablorobertos", 
         password: "********",
         host: "smtp.gmail.com",
         ssl: true
     });
 
-db.open(function (err, db) {});
+Db.connect("mongodb://localhost:27017/?w=1", function (err, db) {
+    exports.authenticate = function (key, callback) {
+        db.collection("guests", function (error, collection) {
+            collection.findOne({key: key}, callback);
+        });
+    };
 
-exports.authenticate = function (key, callback) {
-    db.collection("guests", function (error, collection) {
-        collection.findOne({key: key}, callback);
-    });
-};
+    exports.rsvp = function (key, rsvpValue, callback) {
+        var criteria = {key: key },
+            update = {$push: {}},
+            options = {"new": true, "upsert": false};
 
-exports.rsvp = function (key, rsvpValue, callback) {
-    var criteria = {key: key },
-        update = {$push: {}},
-        options = {"new": true, "upsert": false};
-
-    db.collection("guests", function (error, collection) {
-        collection.findOne({key: key}, function (err, guest) {
-            Object.keys(guest.people).forEach(function (person) {
-                update.$push["people." + person] = rsvpValue[person];
-            });
-
-            collection.findAndModify(criteria, [["_id", "asc"]], update, options, function (error, result) {
-                server.send({
-                    text: "The value posted: \n\n" + JSON.stringify(rsvpValue), 
-                    from: "paulandkana.com <pablorobertos@gmail.com>", 
-                    to: "Paul Roberts <psxpaul@gmail.com>, Kana Miyata <myt.kana@gmail.com>",
-                    subject: "Someone RSVP'd for your wedding!"
-                }, function (emailErr, message) {
-                    console.log(emailErr || message);
+        db.collection("guests", function (error, collection) {
+            collection.findOne({key: key}, function (err, guest) {
+                Object.keys(guest.people).forEach(function (person) {
+                    update.$push["people." + person] = rsvpValue[person];
                 });
-                callback(error, result);
+
+                collection.findAndModify(criteria, [["_id", "asc"]], update, options, function (error, result) {
+                    emailServer.send({
+                        text: "The value posted: \n\n" + JSON.stringify(rsvpValue), 
+                        from: "paulandkana.com <pablorobertos@gmail.com>", 
+                        to: "Paul Roberts <psxpaul@gmail.com>, Kana Miyata <myt.kana@gmail.com>",
+                        subject: "Someone RSVP'd for your wedding!"
+                    }, function (emailErr, message) {
+                        console.log(emailErr || message);
+                    });
+                    callback(error, result);
+                });
             });
         });
-    });
-};
+    };
 
-exports.findAll = function (key, callback) {
-    db.collection("guests", function (err, collection) {
-        collection.find().toArray(callback);
-    });
-};
+    exports.findAll = function (key, callback) {
+        db.collection("guests", function (err, collection) {
+            collection.find().toArray(callback);
+        });
+    };
+});
